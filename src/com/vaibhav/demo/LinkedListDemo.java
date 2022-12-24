a90e7c4388144ad1bc954ae8e40f84305b9410d1
vaibhavdhavare <vaibhavdhavare1@gmail.com>
2022-12-24 17:56:15 +0530

    Second commit : new code

---
+++ src/com/vaibhav/demo/LinkedListDemo.java
@@ -0,0 +1,15 @@
+package com.vaibhav.demo;
+
+import java.util.LinkedList;
+
+public class LinkedListDemo {
+	public static void main(String[] args) {
+		LinkedList<Integer>list=new LinkedList<>();
+		list.add(1);
+		list.add(2);
+		list.add(3);
+		System.out.println(list);
+		
+	}
+
+}
