22592a2feabf27be393b16c323f9bf57627d45e8
vaibhavdhavare <vaibhavdhavare1@gmail.com>
2022-12-24 17:35:46 +0530

    First commit : added arrayList demo code

---
+++ src/com/vaibhav/demo/ArrayListDemo.java
@@ -0,0 +1,17 @@
+package com.vaibhav.demo;
+
+import java.util.ArrayList;
+
+public class ArrayListDemo {
+	public static void main(String[] args) {
+		
+	
+	ArrayList <String> list=new ArrayList<String>();
+	list.add("Vaibhav");
+	list.add("Ashutoh");
+	list.add("Kapil");
+	
+	System.out.println(list);
+
+}
+}
